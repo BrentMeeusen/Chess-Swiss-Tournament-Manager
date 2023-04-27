7a29318bd6eba8c6d5fb07bf0b32991c449a98f9
BrentMeeusen <brentm@live.nl>
2023-04-27 20:32:35 +0200

    Fix matchmaking bugs

--- src/main/java/com/rds/stm/swiss/Round.java
+++ src/main/java/com/rds/stm/swiss/Round.java
@@ -1,6 +1,7 @@
 package com.rds.stm.swiss;
 
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class Round {
 
@@ -10,7 +11,7 @@ public class Round {
 
 	public Round(int round, ArrayList<Player> players) {
 		this.round = round;
-		this.players = (ArrayList<Player>) players.stream().sorted().toList();
+		this.players = (ArrayList<Player>) players.stream().sorted().collect(Collectors.toList());
 		this.matches = findMatches();
 	}
 
@@ -25,16 +26,24 @@ public class Round {
 	 */
 	private ArrayList<Match> findMatches() {
 		ArrayList<Match> matches = new ArrayList<>();
+		ArrayList<Player> matchedPlayers = new ArrayList<>();
+
 		for(int i = 0; i < players.size(); i++) {
 			Player p1 = players.get(i);
+			if(matchedPlayers.contains(p1)) continue;
+
 			for(int j = i + 1; j < players.size(); j++) {
 				Player p2 = players.get(j);
+				if(matchedPlayers.contains(p2)) continue;
 				Match m = new Match(p1, p2);
 
 				// If they haven't played against each other in this set of rounds yet, schedule the match
 				if(p1.getMatchesAgainst(p2).size() == ((round - 1) / (players.size() - 1)) &&
 						!matches.contains(m)) {
 					matches.add(m);
+					matchedPlayers.add(p1);
+					matchedPlayers.add(p2);
+					break;
 				}
 			}
 		}
