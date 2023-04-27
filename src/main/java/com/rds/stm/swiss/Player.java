f6cc2aef2a51b9a3f0e786185d431b2d1228fe17
BrentMeeusen <brentm@live.nl>
2023-04-27 16:54:39 +0200

    Add tests

--- src/main/java/com/rds/stm/swiss/Player.java
+++ src/main/java/com/rds/stm/swiss/Player.java
@@ -12,4 +12,16 @@ public class Player {
 		this.name = name;
 	}
 
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 }
