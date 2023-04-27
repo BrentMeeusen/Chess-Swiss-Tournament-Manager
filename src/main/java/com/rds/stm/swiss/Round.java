69ccec3b7b211e21681d7c8b0e2e2cc68d27400d
BrentMeeusen <brentm@live.nl>
2023-04-27 17:47:05 +0200

    Create Round constructor

--- src/main/java/com/rds/stm/swiss/Round.java
+++ src/main/java/com/rds/stm/swiss/Round.java
@@ -1,5 +1,13 @@
 package com.rds.stm.swiss;
 
+import java.util.ArrayList;
+
 public class Round {
+
+	ArrayList<Player> players;
+
+	public Round(ArrayList<Player> players) {
+		this.players = (ArrayList<Player>) players.stream().sorted().toList();
+	}
 
 }
--- src/main/java/com/rds/stm/swiss/Tournament.java
+++ src/main/java/com/rds/stm/swiss/Tournament.java
@@ -3,25 +3,33 @@ package com.rds.stm.swiss;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.LinkedList;
 
 public class Tournament implements Serializable {
 
 	private ArrayList<Player> players;
-	private int round;
+	private LinkedList<Round> rounds;
 
 	public Tournament() {
 		players = new ArrayList<>();
-		round = 0;
+		rounds = new LinkedList<>();
 	}
 
 	public ArrayList<Player> getPlayers() {
 		return players;
 	}
 
-	public void startNewRound() {
-		// Sort players based on score, then rating
+	public Round startNewRound() {
+
+		// Start a new round with the players
+		Round round = new Round(players);
+
 		// If uneven number of players, pick random substitute player (that's not been picked yet)
 		// Match to the closest player they haven't met yet
+
+		// Add round to list and return it
+		rounds.addLast(round);
+		return round;
 
 	}
 
