da5ddb1f2a641bd2f0ad30d4d2987d7c7f7af9af
BrentMeeusen <brentm@live.nl>
2023-04-27 17:13:58 +0200

    Add methods for adding and removing players to and from the tournament

--- src/main/java/com/rds/stm/swiss/Tournament.java
+++ src/main/java/com/rds/stm/swiss/Tournament.java
@@ -2,6 +2,7 @@ package com.rds.stm.swiss;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 
 public class Tournament implements Serializable {
 
@@ -9,6 +10,30 @@ public class Tournament implements Serializable {
 
 	public Tournament() {
 		players = new ArrayList<>();
+	}
+
+	/**
+	 * Adds a player to the tournament.
+	 *
+	 * @param player The player to add
+	 * @throws InputMismatchException If the player's name is not unique
+	 */
+	public void addPlayer(Player player) {
+		if(players.stream().filter(p -> p.getName().equals(player.getName())).toList().size() == 0) {
+			players.add(player);
+		}
+		else {
+			throw new InputMismatchException("Duplicate names cannot exist.");
+		}
+	}
+
+	/**
+	 * Removes the player from the tournament if the player was found.
+	 *
+	 * @param player The player to remove
+	 */
+	public void removePlayer(Player player) {
+		players.remove(player);
 	}
 
 }
