0757e05a2cdada007722a8e1b962c7979378df80
BrentMeeusen <brentm@live.nl>
2023-04-27 18:03:58 +0200

    Add tests for sorting

--- src/test/java/com/rds/stm/swiss/PlayerTest.java
+++ src/test/java/com/rds/stm/swiss/PlayerTest.java
@@ -1,17 +1,60 @@
 package com.rds.stm.swiss;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class PlayerTest {
 
-	@Test
-	public void testPlayerIds() {
-		Player p1 = new Player("Alice", 800);
-		Player p2 = new Player("Bob", 800);
+	public Player[] getPlayers() {
+		return new Player[] {
+			new Player("Alice", 500),
+			new Player("Bob", 800),
+			new Player("Chris", 600),
+			new Player("Delilah", 900),
+			new Player("Eric", 500)
+		};
+	}
 
-		assertEquals(1, p1.getId());
-		assertEquals(2, p2.getId());
+	public List<Player> sortPlayers(Player[] players) {
+		return new ArrayList<>(List.of(players)).stream().sorted().toList();
+	}
+
+	@Test
+	public void testSortedOrderWithoutScore() {
+		Player[] players = getPlayers();
+		List<Player> sorted = sortPlayers(players);
+		assertEquals(players.length, sorted.size());
+
+		for(int i = 0; i < sorted.size() - 1; i++) {
+			assertTrue(sorted.get(i).getRating() <= sorted.get(i + 1).getRating());
+		}
+	}
+
+	@Test
+	public void testSortedOrderWithScores() {
+		Player[] players = getPlayers();
+		players[0].addResult(MatchResult.WIN);
+		players[1].addResult(MatchResult.DRAW);
+		List<Player> sorted = sortPlayers(players);
+
+		int[] expected = { 4, 2, 3, 1, 0 };
+		assertEquals(expected.length, sorted.size());
+
+		for(int i = 0; i < expected.length; i++) {
+			assertEquals(players[expected[i]], sorted.get(i));
+		}
+
+		players[3].addResult(MatchResult.WIN);
+		sorted = sortPlayers(players);
+		expected = new int[] {4, 2, 1, 0, 3};
+
+		for(int i = 0; i < expected.length; i++) {
+			assertEquals(players[expected[i]], sorted.get(i));
+		}
+
 	}
 
 }
