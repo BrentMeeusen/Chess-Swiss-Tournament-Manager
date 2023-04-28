61dd751d05cf7b95fa8452c426891bd2b68b881f
BrentMeeusen <brentm@live.nl>
2023-04-28 13:55:20 +0200

    Add and remove players from tournament

--- src/main/java/com/rds/stm/SwissTournamentApplication.java
+++ src/main/java/com/rds/stm/SwissTournamentApplication.java
@@ -13,7 +13,7 @@ public class SwissTournamentApplication extends Application {
 	public void start(Stage stage) throws IOException {
 		FXMLLoader fxmlLoader =
 			new FXMLLoader(SwissTournamentApplication.class.getResource("hello-view.fxml"));
-		Scene scene = new Scene(fxmlLoader.load(), 800, 500);
+		Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
 		stage.setTitle("Chess Swiss Tournament Manager");
 		stage.setScene(scene);
 		stage.show();
--- src/main/java/com/rds/stm/SwissTournamentController.java
+++ src/main/java/com/rds/stm/SwissTournamentController.java
@@ -1,8 +1,72 @@
 package com.rds.stm;
 
+import com.rds.stm.swiss.Player;
+import com.rds.stm.swiss.Tournament;
+import java.text.ParseException;
+import java.util.InputMismatchException;
+import javafx.event.Event;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 
 public class SwissTournamentController {
+
+	private Tournament tournament = new Tournament();
+
+	@FXML
+	private TextField playerName;
+
+	@FXML
+	private TextField playerRating;
+
+	@FXML
+	private ListView<Player> playerList;
+
+	/**
+	 * Accessed from "Add player" button.
+	 */
+	public void addPlayer() {
+		try {
+			Player player = new Player(playerName.getText(), Integer.parseInt(playerRating.getText()));
+			tournament.addPlayer(player);
+		}
+		catch(InputMismatchException | NumberFormatException e) {
+			Alert alert = new Alert(Alert.AlertType.ERROR);
+			alert.setContentText(e.getMessage());
+			alert.show();
+			return;
+		}
+		showPlayers();
+	}
+
+	/**
+	 * Accessed from keyboard press.
+	 */
+	public void removePlayer(KeyEvent e) {
+		System.out.println("Pressed " + e.getCode());
+		if(e.getCode() == KeyCode.DELETE) {
+			Player selected = playerList.getSelectionModel().getSelectedItem();
+			tournament.removePlayer(selected);
+			showPlayers();
+		}
+	}
+
+	/**
+	 * Accessed from the menu bar.
+	 */
+	public void createNewTournament() {
+		tournament = new Tournament();
+		showPlayers();
+	}
+
+	private void showPlayers() {
+		playerList.getItems().clear();
+		for(Player player : tournament.getPlayers()) {
+			playerList.getItems().add(player);
+		}
+	}
 
 }
